a110ae53559e341369771e792516a906777a05a4
Samy Pesse <samypesse@gmail.com>
2015-04-18 22:08:47 -0500

    Fix and add test for widgets

--- tests/index.js
+++ tests/index.js
@@ -6,20 +6,59 @@ describe('VirtualView', function() {
 	$testBody.appendTo($("body"));
 
 	describe('Rendering', function() {
-		var V = VView.extend({});
-		var v = new V();
-		v.appendTo($testBody);
+		var V, v;
+
+		it('should correctly create a view', function() {
+			V = VView.extend({
+				tagName: "p"
+			});
+			v = new V();
+			v.appendTo($testBody);
+		});
 
 		it('can correctly be rendered as html', function() {
 			v.html("<b>hello</b>");
-			expect($testBody.html()).to.equal("<div><b>hello</b></div>");
+			expect($testBody.html()).to.equal("<p><b>hello</b></p>");
 		});
 
 		it('can correctly be rendered as updated', function() {
 			v.html("<b>hello 2</b>");
-			expect($testBody.html()).to.equal("<div><b>hello 2</b></div>");
+			expect($testBody.html()).to.equal("<p><b>hello 2</b></p>");
 		});
 
+		it('can correctly be removed', function() {
+			v.remove();
+			v.$el.remove();
+			expect($testBody.html()).to.equal("");
+		});
+
+
+	});
+
+	describe('Widget', function() {
+		var V, v, TestWidget;
+
+		it('should correctly create a view', function() {
+			TestWidget =  VView.CustomElementWidget(function(elem) {
+				var container = document.createElement("span");
+	            container.textContent = "Hello "+elem.textContent;
+	            return container;
+			});
+			V = VView.extend({
+				patchVirtualElement: function(el) {
+					if (el.tagName == "TEST") {
+						return new TestWidget(el);
+					}
+				}
+			});
+			v = new V();
+			v.appendTo($testBody);
+		});
+
+		it('should correctly be replaced', function() {
+			v.html("This is a test, <test>World</test>");
+			expect($testBody.html()).to.equal("<div>This is a test, <span>Hello World</span></div>");
+		});
 	});
 
 });
